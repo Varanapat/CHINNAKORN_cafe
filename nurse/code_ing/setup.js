73e2617d23dc6a591c25b85f7e4fb68cb8b3a92d
Varanapat <varanapat3458@gmail.com>
2025-10-11 23:40:02 +0700

    nurse

---
+++ nurse/code_ing/setup.js
@@ -0,0 +1,21 @@
+const { execSync } = require("child_process");
+const os = require("os");
+
+console.log("Detecting OS...");
+
+const platform = os.platform(); // 'win32' = Windows, 'darwin' = Mac, 'linux' = Linux
+
+try {
+  console.log("Initializing project...");
+  execSync("npm init -y", { stdio: "inherit" });
+
+  console.log("Installing dependencies...");
+  execSync("npm install promptpay-qr qrcode express ejs express-session cookie-parser nodemon sqlite3", { stdio: "inherit" });
+
+  console.log("✅ Setup completed successfully!");
+} catch (err) {
+  console.error("❌ Error running setup:", err);
+}
+
+
+// node setup.js
